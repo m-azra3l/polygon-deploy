2c9a5e0668bf831ca690cf17c79f0cba90479873
Michael <gadreelazazel@gmail.com>
2023-05-29 22:25:00 +0100

    create

---
+++ src/Contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+contract Contract {
+  function Hello() public pure returns (string memory) { 
+      return "Hello From Polygon"; 
+  }
+}
